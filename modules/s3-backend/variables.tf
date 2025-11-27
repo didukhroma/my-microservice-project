fb48ed28130b79a44075e591c7bbd2809ef9be3f
Roman <didukhroma@gmail.com>
2025-11-27 18:53:42 +0100

    feat: created dev barnch with docker

--- modules/s3-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "S3 bucket name for Terraform state"
-  type        = string
-}
-
-variable "table_name" {
-  description = "DynamoDB table name for state locking"
-  type        = string
-}
